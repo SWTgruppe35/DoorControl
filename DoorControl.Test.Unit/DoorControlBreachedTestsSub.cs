219f36cf25c00cf3084f3dab09c89abe433dfa79
Nichlaes <nichlaes.soerensen@gmail.com>
2019-02-20 10:23:00 +0100

    Substitute added

---
+++ DoorControl.Test.Unit/DoorControlBreachedTestsSub.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DoorControl.Test.Unit.Mocks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DoorControl.Test.Unit
+{
+    // Test Fixture for exception 2: Door breached (i.e. opened without prior validation)
+    [TestFixture]
+    public class DoorControlDoorBreachedTestsSub
+    {
+        private IDoorControlFactory _doorControlFactory;
+        private DoorControl _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _doorControlFactory = Substitute.For<IDoorControlFactory>();
+
+            _uut=new DoorControl(_doorControlFactory);
+        }
+
+        [Test]
+        public void DoorBreached_AlarmIsCalled()
+        {
+            _uut.DoorOpened();
+            _doorControlFactory.Received().CreateAlarm();
+
+        }
+    }
+
+}
